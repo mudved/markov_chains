15489b3625878172ec3df443c5f683bdb520a184
mudved <lislislis1986@gmail.com>
2019-03-19 15:13:47 +0800

    SQL

--- sql.py
+++ sql.py
@@ -1,53 +1,81 @@
 import sqlite3
 
-conn = sqlite3.connect("testdatabase.db")
-cursor = conn.cursor()
+def create_db(order=3):
+    """Создаёт базу данных для хранения модели цепи Маркова
+    order - порядок цепи"""
 
-sql = """
-CREATE TABLE tag(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-tag TEXT);
+    name_db = 'markovdb_order'+str(order)
+    conn = sqlite3.connect(name_db)
+    cursor = conn.cursor()
 
-CREATE TABLE category(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-category TEXT);
+    sql = """
+    CREATE TABLE tag(
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    tag TEXT);
 
-CREATE TABLE word(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-word TEXT);
+    CREATE TABLE category(
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    category TEXT);
 
-CREATE TABLE window_3(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-word_1 TEXT,
-word_2 TEXT,
-word_3 TEXT);
+    CREATE TABLE word(
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    word TEXT);
 
-CREATE TABLE window_3_word(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-word_id INTEGER,
-window_id INTEGER,
-count INTEGER);
 
-CREATE TABLE w3w_tag(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-w3w_id INTEGER,
-tag_id INTEGER);
+    CREATE TABLE window_word(
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    word_id INTEGER NOT NULL REFERENCES word(id),
+    window_id INTEGER NOT NULL REFERENCES window(id),
+    count INTEGER);
 
-CREATE TABLE w3w_category(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-w3w_id INTEGER,
-category_id);"""
+    CREATE TABLE ww_tag(
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    ww_id INTEGER,
+    tag_id INTEGER);
 
-#cursor.execute("""CREATE TABLE WINDOW_3
-#        (ID integer, WORD_1 text, WORD_2 text, WORD_3 text)""")
+    CREATE TABLE ww_category(
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    ww_id INTEGER,
+    category_id);
+      
+    CREATE TABLE window(
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"""
+     
+    if order == 1:
+        sql += """
+                word_1 TEXT);
+                """
+    elif order == 2:
+        sql += """
+                word_1 TEXT,
+                word_2 TEXT);
+                """
+    elif order == 3:
+        sql += """
+                word_1 TEXT,
+                word_2 TEXT,
+                word_3 TEXT);
+                """
+    elif order == 4:
+        sql += """
+                word_1 TEXT,
+                word_2 TEXT,
+                word_3 TEXT,
+                word_4 TEXT);
+                """
 
-try:
-    cursor.executescript(sql)
-except sqlite.DatabaseError as err:
-    print("Ошибка: ", err)
-else:
-    print("Запрос выполнен успешно")
+    try:
+        cursor.executescript(sql)
+    except sqlite3.DatabaseError as err:
+        print("Ошибка: ", err)
+    else:
+        print("Запрос выполнен успешно")
 
-conn.commit()
-cursor.close()
-conn.close()
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+if __name__ == '__main__':
+    create_db()
+    create_db(1)
+    create_db(2)
