ed68368fd4f5dd5d791222d483f9b9c824594630
mudved <lislislis1986@gmail.com>
2019-03-22 00:53:40 +0800

    update parser (proxy)

--- mudved_parser.py
+++ mudved_parser.py
@@ -1,10 +1,11 @@
 import requests
+from random import choice
 from bs4 import BeautifulSoup
 
-def get_html(url):
-    '''Получаем текст html страницы по url-адресу'''
+def get_html(url, useragent=None, proxy=None):
+    '''Получает текст html страницы по url-адресу'''
 
-    r = requests.get(url)
+    r = requests.get(url, headers=useragent, proxies=proxy)
     return r.text
 
 def get_ip(html):
@@ -16,21 +17,39 @@ def get_ip(html):
 
 
 def get_proxylist():
+    '''Парсит список прокси и портов и записывает в файл
+    возвращает True, если удалось записать хотя бы 1 прокси'''
+
     url = 'https://free-proxy-list.net'
     html = get_html(url)
     soup = BeautifulSoup(html, 'lxml')
-    for i in range(1, 20):
-        ip = soup.findAll('tr')[i].next_sibling('td')[0].text.strip()
-        port =soup.findAll('tr')[i].next_sibling('td')[1].text.strip()
-        print(ip)
-        print(port)
 
+    with open(r'parser_data\proxies.txt', 'w') as f:
+        first = True
+        for i in range(1, 20):
+            ip = soup.findAll('tr')[i].next_sibling('td')[0].text.strip()
+            port = soup.findAll('tr')[i].next_sibling('td')[1].text.strip()
+            yes =  soup.findAll('tr')[i].next_sibling('td')[6].text.strip()
+            if yes == 'no' and not first:
+                proxy = '\n{0}:{1}'.format(ip, port)
+                f.write(proxy)
+            elif yes == 'no' and first:
+                proxy = '{0}:{1}'.format(ip, port)
+                first = False
+                f.write(proxy)
 
+    return not first
 
 def main():
-    #html = get_html('http://sitespy.ru/my-ip')
-    #get_ip(html)
-    get_proxylist()
+    #get_proxylist()
+    useragents = open(r'parser_data\useragents.txt').read().split('\n')
+    proxies = open(r'parser_data\proxies.txt').read().split('\n')
+
+    proxy = {'http': 'http://' + choice(proxies)}
+    useragent = {'User-Agent': choice(useragents)}
+    html = get_html('http://sitespy.ru/my-ip', useragent, proxy)
+    get_ip(html)
+
 
 if __name__ == '__main__':
     main()
