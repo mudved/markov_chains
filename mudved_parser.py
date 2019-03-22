6f7db8ba3635353bf8875158e76b10a4e6d802ee
mudved <lislislis1986@gmail.com>
2019-03-22 18:14:57 +0800

    Update parser

--- mudved_parser.py
+++ mudved_parser.py
@@ -15,11 +15,47 @@ def get_ip(html):
     print(ip)
     print(ua)
 
+def parser(url, donor, use_proxy=False):
+    '''Функция для парсинга страниц доноров'''
+
+    print("Start parsing: ", url)
+    if use_proxy and get_proxylist():
+        print("Proxy parsing completed.")
+        useragents = open(r'parser_data\useragents.txt').read().split('\n')
+        proxies = open(r'parser_data\proxies.txt').read().split('\n')
+        for i in range(10):
+            proxy = {'http': 'http://' + choice(proxies)}
+            useragent = {'User-Agent': choice(useragents)}
+            try:
+                html = get_html(url, useragent, proxy)
+            except:
+                continue
+            else:
+                print("Used proxy: ", proxy)
+                break
+    else:
+        print("Proxy not use.")
+        html = get_html(url)
+
+    soup = BeautifulSoup(html, 'lxml')
+    
+    title= soup.find('title').text.strip()
+    description = soup.find('meta', {"name":"description"})['content']
+    video = soup.find('meta', {"property":"og:video"})['content']
+    image = soup.find('meta', {"property":"og:image"})['content']
+    h1 = soup.find('span', id = "news-title").text.strip()
+    content = soup.find('div', class_="post_content cf").text.strip()
+    category = soup.find_all('span', itemprop="title")[1].text.strip()
+    tags = soup.find('meta', {"name":"news_keywords"})['content'].split(',')
+    result = {'h1':h1, 'title':title, 'description':description, 'video':video, 'image':image, 'content':content, 'category':category, 'tags':tags}
+
+    return result
 
 def get_proxylist():
     '''Парсит список прокси и портов и записывает в файл
     возвращает True, если удалось записать хотя бы 1 прокси'''
 
+    print("Start proxy parsing")
     url = 'https://free-proxy-list.net'
     html = get_html(url)
     soup = BeautifulSoup(html, 'lxml')
@@ -41,14 +77,25 @@ def get_proxylist():
     return not first
 
 def main():
-    #get_proxylist()
-    useragents = open(r'parser_data\useragents.txt').read().split('\n')
-    proxies = open(r'parser_data\proxies.txt').read().split('\n')
-
-    proxy = {'http': 'http://' + choice(proxies)}
-    useragent = {'User-Agent': choice(useragents)}
-    html = get_html('http://sitespy.ru/my-ip', useragent, proxy)
+    if True or get_proxylist():
+        print("Proxy parsing completed.")
+        useragents = open(r'parser_data\useragents.txt').read().split('\n')
+        proxies = open(r'parser_data\proxies.txt').read().split('\n')
+        for i in range(10):
+            proxy = {'http': 'http://' + choice(proxies)}
+            useragent = {'User-Agent': choice(useragents)}
+            try:
+                html = get_html('http://sitespy.ru/my-ip', useragent, proxy)
+            except:
+                continue
+            else:
+                break
+    else:
+        print("Proxy parsing not completed.")
+        html = get_html('http://sitespy.ru/my-ip')
     get_ip(html)
+    result = parser('http://pornolomka.me/8285-pizda-krupno-posle-seksa.html', 'pornolomka.me')
+    print(result)
 
 
 if __name__ == '__main__':
